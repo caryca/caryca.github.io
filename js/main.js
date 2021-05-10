02c4f66bc9d710d3a7cabb254d452012936e65ce
Lidia Caryca <hetane@gmail.com>
2021-05-10 19:15:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 	/*kiedy nastąpi kliknięcie elementu o klasie .js-toggle-nav, czyli nasz guzik - button, wtedy ...*/
-	$('.js-toggle-nav').click(function () {
+	$('js-toggle-nav').click(function () {
 	/*...element o klasie .js-menu zostanie rozwinięty*/
-    $('.js-menu').slideToggle();
+    $('js-menu').slideToggle();
     /* jeśli wyświetlany tekst po kliknięciu na guziku będzie 'x', wtedy zamień go na 'menu'*/
     if ($(this).text() == "x")
        $(this).text("Menu")
